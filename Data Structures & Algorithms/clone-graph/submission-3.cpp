1603d0ec62afd59989d70675ced409212852efd4
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-08-12 15:08:35 -0700

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return node;
+
+        unordered_map<Node*, Node*> oldToNew;
+        oldToNew[nullptr] = nullptr;
+        queue<Node*> q;
+        q.push(node);
+        while(!q.empty()){
+            Node* n = q.front();
+            q.pop();
+
+            if(!oldToNew.contains(n)){
+                oldToNew[n] = new Node(n->val);
+            }
+
+            vector<Node*> neighbors;
+            for(auto& nei : n->neighbors){
+                if(!oldToNew.contains(nei)){
+                    q.push(nei);
+                    oldToNew[nei] = new Node(nei->val);
+                }
+                neighbors.push_back(oldToNew[nei]);
+            }
+            oldToNew[n]->neighbors = neighbors;
+        }
+        return oldToNew[node];
+    }
+};
