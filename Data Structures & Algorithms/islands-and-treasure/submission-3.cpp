2b53e16021896a550acbfa1ffc2cb7b32011bee9
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-08-26 10:21:14 -0700

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int directions[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+    int INF = INT_MAX;
+    void bfs(vector<vector<int>>& grid, queue<pair<int, int>> q){
+        while(!q.empty()){
+            auto [r, c] = q.front();
+            q.pop();
+            for(auto d : directions){
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr < 0 || nc < 0 || nr >= grid.size() || nc >= grid[0].size() ||
+                    grid[nr][nc] != INF) continue;
+
+                q.push({nr, nc});
+                grid[nr][nc] = 1 + grid[r][c]; 
+            }
+        }
+    }
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        for(int r = 0; r < grid.size(); r++){
+            for(int c = 0; c < grid[0].size(); c++){
+                if(grid[r][c] == 0){
+                    q.push({r,c});
+                }
+            }
+        }
+
+        bfs(grid, q);
+    }
+};
