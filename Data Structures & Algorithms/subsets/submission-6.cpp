28fe6b39d8a7d1b9cea83b3c0e354ea050e22c08
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-09-13 16:19:33 -0700

    Add: subsets - submission-6

---
+++ Data Structures & Algorithms/subsets/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+        for(int& n : nums){
+            int size = res.size();
+            for(int i = 0; i < size; i++){
+                vector<int> subset = res[i];
+                subset.push_back(n);
+                res.push_back(subset);
+                subset.pop_back();
+            }
+        }
+        return res;
+    }
+};
