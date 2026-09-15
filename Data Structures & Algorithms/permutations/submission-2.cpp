5ac8fdc99c3687538d3e9e4b5913b99da3f5874d
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-09-15 13:19:48 -0700

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void backtrack(vector<int>& nums, vector<int>& cur, vector<bool>& visited){
+        if(cur.size() == nums.size()){
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(!visited[i]){
+                cur.push_back(nums[i]);
+                visited[i] = true;
+                backtrack(nums, cur, visited);
+
+                cur.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> visited(nums.size(), false);
+        vector<int> cur;
+        backtrack(nums, cur, visited);
+        return res;
+    }
+};
