b9aa5e88e549104b895751fbbabde28b8e2c4403
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-09-16 08:55:10 -0700

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool backtrack(vector<vector<char>>& board, string word, int r, int c, int idx){
+        if(r < 0 || r >= board.size() ||
+            c < 0 || c >= board[0].size() ||
+            board[r][c] != word[idx])
+            return false;
+        
+        char cha = board[r][c];
+        board[r][c] = '#';
+        bool res = false;
+
+        if(idx == word.size() - 1)
+            return true;
+        idx++;
+        res = backtrack(board, word, r - 1, c, idx) ||
+                backtrack(board, word, r + 1, c, idx) ||
+                backtrack(board, word, r, c - 1, idx) || 
+                backtrack(board, word, r, c + 1, idx);
+        board[r][c] = cha;
+        return res;        
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int r = 0; r < board.size(); r++){
+            for(int c = 0; c < board[0].size(); c++){
+                if(backtrack(board, word, r, c, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
