d226b9aead05c13465cf5ff631e5bf6291d349f8
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-09-16 08:36:30 -0700

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void backtrack(vector<int>& nums, vector<int>& cur, int idx){
+        res.push_back(cur);
+
+        for(int i = idx; i < nums.size(); i++){
+            if(i > idx && nums[i] == nums[i-1])
+                continue;
+            
+            cur.push_back(nums[i]);
+            backtrack(nums, cur, i + 1);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> cur;
+        backtrack(nums, cur, 0);
+        return res;
+    }
+};
