8197cc16aacf33d7688f53cd6eb8ef7affe1dec3
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-09-16 09:01:52 -0700

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+    bool isPalindrome(string s){
+        if(s.empty())
+            return false;
+        
+        int i = 0;
+        int j = s.size() - 1;
+        while(i < j){
+            if(s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void backtrack(string& s, vector<string>& cur, int idx){
+        if(idx == s.size()){
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i = idx; i < s.size(); i++){
+            string pat = s.substr(idx, i - idx + 1);
+            if(isPalindrome(pat)){
+                cur.push_back(pat);
+                backtrack(s, cur, i + 1);
+                cur.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> cur;
+        backtrack(s, cur, 0);
+        return res;
+    }
+};
