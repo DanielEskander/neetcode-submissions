601ab2fb31096f7e5c8a8300fb4d815f8016663a
Daniel Eskander <55925126+DanielEskander@users.noreply.github.com>
2026-09-17 15:31:23 -0700

    Add: combination-target-sum - submission-9

---
+++ Data Structures & Algorithms/combination-target-sum/submission-9.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void backtrack(vector<int>& nums, int target, vector<int>& cur, int idx){
+        if(target == 0){
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i = idx; i < nums.size(); i++){
+            if(target - nums[i] < 0)
+                continue;
+            cur.push_back(nums[i]);
+            backtrack(nums, target - nums[i], cur, i);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> cur;
+        backtrack(nums, target, cur, 0);
+        return res;
+    }
+};
